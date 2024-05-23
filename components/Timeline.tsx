754d2f0cc9d7be61c51027ad70cf4688899b954b
Ali <alimousa787@yahoo.com>
2024-05-22 22:13:04 -0400

    Minor fix for Scaling on mobile platforms.

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -47,7 +47,7 @@ const Timeline: React.FC<TimelineProps> = ({
         });
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
             {filteredMovies.map((movie, index) => (
                 <MovieItem
                     key={index}
